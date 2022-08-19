1316b85162ed516358f6ecb220d7d5fb6f138eba
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-08-18 21:53:19 -0700

    added commits for database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,39 +2,59 @@ import { openDB } from 'idb';
 
 const initdb = async () =>
   openDB('jate', 1, {
+    //creating database called 'jate'
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
+        //checking to see if 'jate' database is already there, if so return
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        //if objectstore 'jate' does not exist, create it and increment it automatically
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  //exports function putDB which will add to database
+
   console.log('Post to the database');
   const contentDb = await openDB('jate', 1);
+  //create connection to 'jate' database
+
   const tx = contentDb.transaction('jate', 'readwrite');
+  //dictates which database on the its data privileges.
+
   const store = tx.objectStore('jate');
+
   const request = store.put({content: content});
+  //update the content
   const result = await request;
   console.log('Data saved to database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  //export function to get data from database
+
   console.log('Get data from database');
   const contentDb = await openDB('jate', 1);
+  //create connection to jate database
+
   const tx = contentDb.transaction('jate', 'readonly');
+  //dictates which database and its data privilege
+
   const store = tx.objectStore('jate');
+
   const request = store.getAll();
+  //gets all the data in the jate database
+
   const result = await request;
   console.log('Data from database', result);
   return result;
 }
 
-//console.error('getDb not implemented');
 
 initdb();
+//start database
